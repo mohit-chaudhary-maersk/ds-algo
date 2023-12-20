ae0cdc87e1dbbd04f2187c45f50cc2980b6c7e33
Mohit Chaudhary <mohit.chaudhary@maersk.com>
2023-12-20 23:25:22 +0530

    array-first
    array-first

---
+++ data-structure-algorithms/src/main/java/org/ds/algo/arrays/Basic.java
@@ -0,0 +1,22 @@
+package org.ds.algo.arrays;
+
+import static java.lang.System.out;
+
+public class Basic {
+
+    public static void main(String[] args) {
+
+        int[] aa = new int[6];
+
+        aa[0] = 54;
+        aa[1] = -2;
+        aa[2] = 78;
+        aa[3] = 153;
+        aa[4] = -49;
+        aa[5] = 540;
+
+        for (int i = 0; i < aa.length; i++) {
+            out.println(aa[i]);
+        }
+    }
+}
